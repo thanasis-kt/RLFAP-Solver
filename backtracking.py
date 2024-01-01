b2a0b714e1e88a3c29aba4a2fbdb1bc8b7e440dd
thanasis-kt <thankotsalis@gmail.com>
2024-01-01 03:13:55 +0200

    cbj (dom/wdeg is probably wrong)

--- backtracking.py
+++ backtracking.py
@@ -17,12 +17,18 @@ class CSP:
         self.constraints = constraints
         self.conDict = conDict
         self.curr_domains = None
-        self.nassigns = 0
-        
+        self.nassigns = 0        
+        #For the dom/wdeg heuristic
         self.weights = {} #Map that stores the weights
                          # for the dom/wdeg heuristic
         for c in conDict:
             self.weights[c] = 1 #Initializing weights
+        
+        self.depth = {} #To track each variables depth (we use dynamic ordering)
+        #For the cbj mechanism
+        self.conflicts = {}
+        for x in variables:
+            self.conflicts[x] = set()
 
     def assign(self,var,val,assignment):
         assignment[(var)] = val
@@ -117,6 +123,11 @@ class CSP:
             self.weights[(var,x)] += 1
             self.weights[(x,var)] += 1
 
+    def conflicted_vars(self, current):
+        """Return a list of variables in current assignment that are in conflict"""
+        return [var for var in self.variables
+                if self.nconflicts(var, current[var], current) > 0]
+
 
 
 #Functions for backtracking search
@@ -206,6 +217,7 @@ def forward_checking(csp,var,value,assignment,removals):
             for b in csp.curr_domains[B][:]:
                 if not csp.constraints(var,value,B,b,csp.conDict):
                     csp.prune(B,b,removals)
+                    csp.conflicts[var].add(B)
             if not csp.curr_domains[B]:
                 csp.revise(var)
                 return False
@@ -218,32 +230,52 @@ import time
 
 def backtracking_search(csp,select_unassigned_variable = dom_wdeg_ordering,order_domain_values = unordered_domain_values,inference = forward_checking):#no_inference):
     startTime = time.time()
-    def backtrack(assignment,startTime):
+    def backtrack(assignment,startTime,depth):
         if len(assignment) == len(csp.variables):
-            return assignment
+            return (assignment,0)
         if (time.time() - startTime > 500):
             print("Didn't finish\n")
             return None
         var = select_unassigned_variable(assignment,csp)
-
+        csp.depth[var] = depth
         for value in order_domain_values(var,assignment,csp):
             if 0 == csp.nconflicts(var,value,assignment):
                 csp.assign(var,value,assignment)
                 removals = csp.suppose(var,value)
                 if inference(csp,var,value,assignment,removals):
-                    result = backtrack(assignment,startTime)
-                    if result is not None:
-                        return result
+                    result = backtrack(assignment,startTime,depth + 1)
+                    if (result[1] > 0):
+                        del csp.depth[var]
+                        csp.unassign(var,assignment)
+                        return (None,result[1] - 1)
+                    if result[0] is not None:
+                        return (result[0],0)
                 csp.restore(removals)
+            # else:
+            #     for b in assignment:
+            #         if not csp.constraints(var,value,b,assignment[b]):
+            #             if b not in csp.conflicts[var]:
+            #                 csp.conflicts[var].append(b)
+
+        del csp.depth[var]
+        maxDepth = 0
+        max = None
+        for b in csp.conflicts[var]:
+
+            if (b in csp.depth and maxDepth < csp.depth[b]):
+                maxDepth = csp.depth[b] 
+                max = b
+        if max != None:
+            csp.conflicts[max] = csp.conflicts[max].union(csp.conflicts[var]) 
         csp.unassign(var,assignment)
         #Domain is empty
-        # csp.revise(var)
+        # csp.revise(var)   
 
-        return None
+        return (None,0)
     import datetime
-    result = backtrack({},startTime)
+    result,depth = backtrack({},startTime,0)
     convert = str(datetime.timedelta(seconds = time.time() - startTime))
-    print("Runnign time: ",convert)
+    print("Running time: ",convert)
     assert result is None or csp.goal_test(result)
     return result
 
@@ -257,15 +289,15 @@ def constraints(A,a,B,b,conDict):
 import os
 
 def my_main():
-    print("we will use file 2-f24\n")
+    print("we will use file 3-f10\n")
     variables = []
     #Creating the list variables
     script_dir = os.path.dirname(os.path.abspath(__file__))
-    file_path = os.path.join(script_dir , "rlfap\\var2-f24.txt")
+    file_path = os.path.join(script_dir , "rlfap\\var3-f10.txt")
     var = open(file_path,"r")
     #str is every line
 
-    file_path = os.path.join(script_dir , "rlfap\dom2-f24.txt")
+    file_path = os.path.join(script_dir , "rlfap\dom3-f10.txt")
     dom = open(file_path,"r")
     #
     #Create the domains
@@ -290,7 +322,7 @@ def my_main():
             first = True
     
     #Create the neighbors and constraints function 
-    file_path = os.path.join(script_dir , "rlfap\ctr2-f24.txt")
+    file_path = os.path.join(script_dir , "rlfap\ctr3-f10.txt")
     ctr = open(file_path,"r")
     neighbors = {}
     conDict = {}
@@ -313,7 +345,58 @@ def my_main():
         
     problem = CSP(variables,domains,neighbors,constraints,conDict)
     print(backtracking_search(problem))
+    # print(min_conflicts(problem))
  
+
+
+def min_conflicts(csp,max_steps = 100000):
+    csp.current = current = {}
+    for var in csp.variables:
+        val = min_conflicts_value(csp,var,current)
+        prev = var
+        csp.assign(var,val,current)
+    for i in range(max_steps):
+        conflicted = csp.conflicted_vars(current)
+        if not conflicted:
+            return current
+        if (i % 100 == 0):
+            print(i,") ",len(conflicted))
+        var = random.choice(conflicted)
+        val = min_conflicts_value(csp,var,current)
+        csp.assign(var,val,current)
+    return None
+
+def min_conflicts_value(csp,var,current):
+    return argmin_random_tie(csp.domains[var], key=lambda val: csp.nconflicts(var, val, current))
+
+
+
+def argmin_random_tie(seq, key):
+    """Return a minimum element of seq; break ties at random."""
+    return min(shuffled(seq), key=key)
+
+def shuffled(iterable):
+    """Randomly shuffle a copy of iterable."""
+    items = list(iterable)
+    random.shuffle(items)
+    return items
+
+
+
+
+
+    # min = []
+    # # minVal = csp.nconflicts(var,val,current)
+    # minVal = 10000000000
+    # for val in csp.domains[var]:
+    #     temp = csp.nconflicts(var,val,current)
+    #     if (minVal > temp):
+    #         min = [val]
+    #         minVal = temp
+    #     elif (minVal == temp):
+    #         min.append(val)
+    # return random.choice(min)
 
 if __name__  == "__main__":
     my_main()
+
